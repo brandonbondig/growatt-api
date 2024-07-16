e619963374ed95632234dcfcc5d2e73c8cf227b1
Brandon Bondig <brandon@bondig.dk>
2024-07-16 23:49:06 +0200

    Added setup

--- setup.py
+++ setup.py
@@ -7,17 +7,17 @@ setup(
     install_requires=[
         'requests'
     ],
-    entry_points={
-        'console_scripts': [
-            'growatt=growatt.growatt:main',  # Assuming you have a main function in growatt.py
-        ],
-    },
+    # entry_points={
+    #     'console_scripts': [
+    #         'growatt=growatt.growatt:main',  # Assuming you have a main function in growatt.py
+    #     ],
+    # },
     author='Brandon Bondig',
     author_email='brandon@bondig.dk',
     description='A Python wrapper for Growatt API',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
-    url='https://github.com/yourusername/growatt',
+    url='https://github.com/brandonbondig/growatt',
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
